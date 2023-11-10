66b5a28aaa4d5cd5c93cd32a66221191b7ac146d
exerebirth <111612783+exerebirth@users.noreply.github.com>
2023-11-10 17:45:24 -0600

    Delete p10/product_app directory

--- p10/product_app/backend/product-search.php
+++
@@ -1,8 +0,0 @@
-<?php
-    use API\Leer\Leer;
-    require_once __DIR__.'/API/start.php';
-
-    $buscar = new Leer('marketzone');
-    $buscar->search( $_GET['search'] );
-    echo $buscar->getResponse();
-?>
