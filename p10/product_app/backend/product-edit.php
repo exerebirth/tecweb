1b1526a964078c756ae280b826524edd63a00f8c
exerebirth <rdo_7_gr@hotmail.com>
2023-11-10 19:09:30 -0600

    se borro parte de la 10

--- p10/product_app/backend/product-edit.php
+++
@@ -1,8 +0,0 @@
-<?php
-    use API\Actualizar\Actualizar;
-    require_once __DIR__.'/API/start.php';
-
-    $actualizar = new Actualizar('marketzone');
-    $actualizar->edit( json_decode( json_encode($_POST) ) );
-    echo $actualizar->getResponse();
-?>
--- p10/product_app/backend/product-list.php
+++
@@ -1,9 +0,0 @@
-<?php
-    use API\Leer\Leer;
-
-    require_once __DIR__.'/API/start.php';
-
-    $leer = new Leer('marketzone');
-    $leer->list();
-    echo $leer->getResponse();
-?>
